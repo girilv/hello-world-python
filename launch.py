b7e4ac48675f7ea45c96546751f7d12af3b7682e
girilv <girilv@gmail.com>
2021-09-05 18:10:29 +0530

    Jenkins pipeline

--- launch.py
+++ launch.py
@@ -2,6 +2,6 @@ from flask import Flask
 helloworld = Flask(__name__)
 @helloworld.route("/")
 def run(): 
-    return "{\"message\":\"Hello World Python Nuvepro frrom Jenkins now\"}"
+    return "{\"message\":\"Hello World Python Nuvepro from Jenkins now\"}"
 if __name__ == "__main__":
     helloworld.run(host="0.0.0.0", port=int("5000"), debug=True)
